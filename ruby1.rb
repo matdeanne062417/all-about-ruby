a2cda13e38fb621b906c56a13ec0937732fa8d8f
matdeanne062417 <matthew.balagtas007@gmail.com>
2020-10-31 13:52:57 +0800

    tried adding files

---
+++ ruby1.rb
@@ -0,0 +1,46 @@
+class Profile
+    attr_accessor :full_name, :age, :work
+    def initialize(full_name, age, address, work)
+      @full_name = full_name
+      @age = age
+      @address = address
+      @work = work
+    end
+  end
+  
+  my_profile = Profile.new('Ana', 18, 'Bulacan', 'Instructor')
+  
+  puts my_profile.full_name
+  my_profile.full_name = 'Analyn Cajocson'
+  my_profile.age = 25
+  my_profile.work = 'Software Engineer'
+  puts my_profile.full_name
+  puts my_profile.age
+  puts my_profile.work
+  
+  class User
+    attr_reader :name
+  
+    def initialize(name)
+      @name = name
+    end
+  
+    def run
+      puts "is running"
+    end
+  
+    def walk
+      puts "is walking"
+    end
+  end
+  
+  class Admin < User
+  end
+  
+  ana = Admin.new("Matt")
+  ana.run
+  ana.walk
+  puts ana.name
+  
+  
+  
