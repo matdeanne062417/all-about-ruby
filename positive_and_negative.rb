3605ffc1c72775bc3f34d3182d9c5c66137599e4
matdeanne062417 <matthew.balagtas007@gmail.com>
2020-11-16 22:57:24 +0800

    added new exercise

--- positive_and_negative.rb
+++ positive_and_negative.rb
@@ -4,17 +4,15 @@ def posNeg(arr)
     if arr.length == 0
         return []
     else
-    for num in arr
-        if (num > 0)
-           count = count + 1 
-        elsif
-            sum = sum + num 
-        else
-            return arr
+        for num in arr
+            if (num > 0)
+               count = count + 1 
+            else
+                sum = sum + num 
+            end
         end
+        return [count, sum]
     end
-    return [count, sum]
-end
 end
 
 puts posNeg([1,2,3,4,5,6,7,8,9,-1,-23])
